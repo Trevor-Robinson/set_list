70c2aef560fa56a32cec8ae22146be96c76d06b1
Trevor Robinson <trevor.robi1254@gmail.com>
2021-02-08 10:22:21 -0700

    switch off

---
+++ spec/features/songs/show_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "song show page" do
+  it 'can show a songs title and artist' do
+    talking_heads = Artist.create!(name: 'Talking Heads')
+    she_was = talking_heads.songs.create(title: 'And She Was', length: 234, play_count: 2994 )
+    wild_life = talking_heads.songs.create(title: 'Wild Wild Life', length: 456, play_count: 384889 )
+
+    visit "/songs/#{she_was.id}"
+
+    expect(page).to have_content(she_was.title)
+    expect(page).to have_content("By: #{talking_heads.name}")
+    expect(page).to_not have_content(wild_life.title)
+  end
+end
